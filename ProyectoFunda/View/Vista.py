b9ce8d3c3afb9bc3b3735cc420ec0fd96febb6dc
Zamo98 <guzamora@estudiantec.cr>
2024-06-10 18:44:26 -0600

    Ventanas

--- ProyectoFunda/View/Vista.py
+++ ProyectoFunda/View/Vista.py
@@ -1,14 +1,45 @@
 import tkinter as tk
-class ventana:
-    def __init__(self,root):
-        self.root=root #esta es la ventana que le pasa tkinter.
-        anchoVentana = 800
-        altoVentana= 600
-        self.root.geometry(f"{anchoVentana}x{altoVentana}") #establece el tamaño de la ventana
-        self.root.resizable(False,False)
+from tkinter import ttk
+import uniciclo
+import multiciclo
+import pipeline1
+import pipeline2
 
-if __name__=="__main__":
-    main=tk.Tk() #esto es la ventana principal
-    main.title("tumagei")#aca se configura el titulo
-    app=ventana(main) #a mi clase le meto la ventana
-    main.mainloop()
+class AplicacionPrincipal(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Simulador de procesadores básicos")
+        self.geometry("800x400")
+
+        self.frame_izquierdo = tk.Frame(self, bg="gray", width=200)
+        self.frame_izquierdo.pack(side="left", fill="y")
+
+        self.label_seleccion = tk.Label(self.frame_izquierdo, text="Seleccionar procesador")
+        self.label_seleccion.pack(pady=20)
+
+        self.procesadores = ["Uniciclo", "Multiciclo", "Segmentado con riesgos y solucionando con stalls","Segmentado con unidad de riesgos y adelantamiento"]
+        self.seleccion = ttk.Combobox(self.frame_izquierdo, values=self.procesadores)
+        self.seleccion.pack(pady=10)
+        self.seleccion.bind("<<ComboboxSelected>>", self.cambiar_ventana)
+
+        self.frame_derecho = tk.Frame(self, bg="navy")
+        self.frame_derecho.pack(side="right", fill="both", expand=True)
+
+        self.label_bienvenida = tk.Label(self.frame_derecho, text="Simulador de procesadores básicos", font=("Arial", 24), fg="white", bg="navy")
+        self.label_bienvenida.pack(expand=True)
+
+    def cambiar_ventana(self, event):
+        seleccion = self.seleccion.get()
+        self.withdraw()
+        if seleccion == "Uniciclo":
+            uniciclo.procesadorUniciclo(self)
+        elif seleccion == "Multiciclo":
+            multiciclo.procesadorMulticiclo(self)
+        elif seleccion == "Segmentado con riesgos y solucionando con stalls":
+            pipeline1.procesadorPipeline(self)
+        elif seleccion == "Segmentado con unidad de riesgos y adelantamiento":
+            pipeline2.procesadorPipeline2(self)
+
+if __name__ == "__main__":
+    app = AplicacionPrincipal()
+    app.mainloop()
